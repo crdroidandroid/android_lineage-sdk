7258034326ef5319d23d499951d4ff175cb2ec90
Pranav Vashi <neobuddy89@gmail.com>
2020-10-07 15:36:09 +0530

    Network traffic mode for status bar [1/3]
    Change-Id: If90bb862d21bfa902db9874f658da2523a063f73
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -2867,30 +2867,25 @@ public final class LineageSettings {
         public static final String LOCK_SCREEN_BLUR_ENABLED = "lock_screen_blur_enabled";
 
         /**
+         * Network traffic indicator location
+         * 0 = Disabled
+         * 1 = Statusbar
+         * 2 = Quick statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_LOCATION = "network_traffic_location";
+
+        /**
          * Network traffic indicator mode
-         * 0 = Don't show network traffic indicator
+         * 0 = Display both up- and down-stream traffic
          * 1 = Display up-stream traffic only
          * 2 = Display down-stream traffic only
-         * 3 = Display both up- and down-stream traffic
          * @hide
          */
         public static final String NETWORK_TRAFFIC_MODE = "network_traffic_mode";
 
         /** @hide */
         public static final Validator NETWORK_TRAFFIC_MODE_VALIDATOR =
-                new InclusiveIntegerRangeValidator(0, 3);
-
-        /**
-         * Network traffic indicator position
-         * 0 = Start side
-         * 1 = Center
-         * 2 = End side
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_POSITION = "network_traffic_position";
-
-        /** @hide */
-        public static final Validator NETWORK_TRAFFIC_POSITION_VALIDATOR =
                 new InclusiveIntegerRangeValidator(0, 2);
 
         /**
@@ -2901,6 +2896,16 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator NETWORK_TRAFFIC_AUTOHIDE_VALIDATOR = sBooleanValidator;
+
+        /**
+         * Threshold below which network traffic would be hidden
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+        /** @hide */
+        public static final Validator NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD_VALIDATOR =
+                new InclusiveIntegerRangeValidator(0, 10000);
 
         /**
          * Measurement unit preference for network traffic
@@ -2920,6 +2925,18 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator NETWORK_TRAFFIC_SHOW_UNITS_VALIDATOR = sBooleanValidator;
+
+        /**
+         * Specify refresh duration for network traffic
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_REFRESH_INTERVAL = "network_traffic_refresh_interval";
+
+        /**
+         * Whether to hide arrows for network traffic
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
 
         /**
          * Trust warnings status
@@ -2984,8 +3001,8 @@ public final class LineageSettings {
             VALIDATORS.put(BERRY_BLACK_THEME, BERRY_BLACK_THEME_VALIDATOR);
             VALIDATORS.put(GESTURE_BACK_EXCLUDE_TOP, GESTURE_BACK_EXCLUDE_TOP_VALIDATOR);
             VALIDATORS.put(NETWORK_TRAFFIC_MODE, NETWORK_TRAFFIC_MODE_VALIDATOR);
-            VALIDATORS.put(NETWORK_TRAFFIC_POSITION, NETWORK_TRAFFIC_POSITION_VALIDATOR);
             VALIDATORS.put(NETWORK_TRAFFIC_AUTOHIDE, NETWORK_TRAFFIC_AUTOHIDE_VALIDATOR);
+            VALIDATORS.put(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD_VALIDATOR);
             VALIDATORS.put(NETWORK_TRAFFIC_UNITS, NETWORK_TRAFFIC_UNITS_VALIDATOR);
             VALIDATORS.put(NETWORK_TRAFFIC_SHOW_UNITS, NETWORK_TRAFFIC_SHOW_UNITS_VALIDATOR);
             VALIDATORS.put(TETHERING_ALLOW_VPN_UPSTREAMS, TETHERING_ALLOW_VPN_UPSTREAMS_VALIDATOR);

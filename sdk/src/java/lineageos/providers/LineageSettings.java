080e6584a67276bf63d325d1fe6f95e9598e8c8f
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-08 12:17:31 +0900

    lineage-sdk: Remove IME space under keyboard feature [2/4]
    Change-Id: Iea104364527be8c4259f5cb5180515e92712a725
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -1072,6 +1072,15 @@ public final class LineageSettings {
                 sBooleanValidator;
 
         /**
+         * boolean value. toggles swipe up hint in gestural nav mode
+         */
+        public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
+
+        /** @hide */
+        public static final Validator NAVIGATION_BAR_IME_SPACE_VALIDATOR =
+                new InclusiveIntegerRangeValidator(0, 2);
+
+        /**
          * Action to perform when the home key is long-pressed.
          * (Default can be configured via config_longPressOnHomeBehavior)
          * 0 - Nothing
@@ -2183,6 +2192,7 @@ public final class LineageSettings {
             VALIDATORS.put(NAVIGATION_BAR_MENU_ARROW_KEYS,
                     NAVIGATION_BAR_MENU_ARROW_KEYS_VALIDATOR);
             VALIDATORS.put(NAVIGATION_BAR_HINT, NAVIGATION_BAR_HINT_VALIDATOR);
+            VALIDATORS.put(NAVIGATION_BAR_IME_SPACE, NAVIGATION_BAR_IME_SPACE_VALIDATOR);
             VALIDATORS.put(KEY_BACK_LONG_PRESS_ACTION, KEY_BACK_LONG_PRESS_ACTION_VALIDATOR);
             VALIDATORS.put(KEY_HOME_LONG_PRESS_ACTION, KEY_HOME_LONG_PRESS_ACTION_VALIDATOR);
             VALIDATORS.put(KEY_HOME_DOUBLE_TAP_ACTION, KEY_HOME_DOUBLE_TAP_ACTION_VALIDATOR);
